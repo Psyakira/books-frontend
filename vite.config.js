cfe536a1f272268bb908437438480c38a296627f
Psyakira <12150121485@students.uin-suska.ac.id>
2023-07-03 17:48:37 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build:{
-    outDir: "build"
-  },
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
